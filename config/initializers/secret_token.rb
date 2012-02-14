4d16c186e1efc253e5c76840c6dd283ddba1a417
Shaan Batra <sb1752@gmail.com>
2012-02-14 13:37:13 -0500

    Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyStashboard::Application.config.secret_token = '0508773db5d504627c322d0add8406605bcc707f60224f49d80de3c3f73b9dccb95ad6017fca0f6752724a390f74fd5bf07921db860d7250171b7f2a22b6caec'

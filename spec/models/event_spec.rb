66502896f3866f25c31e47c18bab4b8cf50f787f
Shaan Batra <sb1752@gmail.com>
2012-02-16 02:00:25 -0500

    event tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Event do
-  describe "New" do
+  describe "new" do
     let(:event) { Event.new }
-
+    
     context "without a message, service_id, or status_id" do  
       it "is not valid" do
         event.should_not be_valid
@@ -30,8 +30,24 @@ describe Event do
         event.save.should be_true
         Event.count.should == 1
       end
-    end  
+    end
   end
   
-  pending "All Days Events"
+  describe "all days events" do
+    # What database does this use? The test database? How do I populate the test database?
+    let(:statuses) { Days.get_all_statuses }
+    let(:services) { Service.all }
+    
+    it "returns the most recent status for each of the last 6 days for every service" do      
+      services.each do |service|
+        6.times do |i|
+          # Get the date to look at and make sure the status is the most recent status past that date 
+          end_time = Time.now.end_of_day - 60*60*24*i
+          recent_status_image = service.statuses.where("events.created_at < #{end_time}").order("events.created_at DESC").first.image
+          recent_status_image.should == statuses[service.name][i]
+        end
+      end
+    end
+    
+  end
 end

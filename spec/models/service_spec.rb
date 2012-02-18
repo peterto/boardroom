396f8432e0bcd613e0d7b4f78dc93aafb09846fb
Josh Laroff <josh@mserve.com>
2012-02-18 12:49:36 -0500

    Added Service spec

---
+++ spec/models/service_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Service do
+  describe "new" do
+    let(:service) { Service.new }
+    
+    context "without a name" do  
+      it "is not valid" do
+        service.should_not be_valid
+      end
+      
+      it "cannot be persisted" do
+        service.save.should be_false
+        service.count.should == 0
+      end
+    end
+    
+    context "with a name" do
+      
+      before(:each) do
+        service.name = 'New Service'
+      end
+        
+      it "is valid" do
+        service.should be_valid
+      end
+      
+      it "can be persisted" do
+        service.save.should be_true
+        Service.count.should == 1
+      end
+      
+      it "is unique" do
+        unique_name_service = Service.new(:name => "New Service")
+        unique_name_service.should_not be_valid
+        unique_name_service.save.should_not be_false
+      end
+    end
+  end
+end

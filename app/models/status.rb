7e68ff54e5455c1b59c910901c1e8c24f61ae1f2
Shaan Batra <sb1752@gmail.com>
2012-02-27 18:49:28 -0500

    everything working

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,7 @@
 class Status < ActiveRecord::Base
   attr_accessible :name, :image, :description
   validates :name, :uniqueness => true, :presence => true
-  validates :image, :presence => true
+  validates :image, :uniqueness => true, :presence => true
   validates :description, :presence => true
   
   has_many :events

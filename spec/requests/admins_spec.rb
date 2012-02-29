abb4737ebe1f5b2852036bdcc36d43ce31c892d2
Peter To <pt9386@gmail.com>
2012-02-29 07:10:35 -0500

    Removed all save_and_open_page from tests

--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -3,18 +3,19 @@ require 'spec_helper'
 feature "Admins" do
   describe "should" do
     scenario "create a new user when not logged in" do
-      visit services_path
+      visit root_path
+      click_link "Admin"
       
       click_link "Sign up"
-      save_and_open_page
+      # save_and_open_page
       
       fill_in 'admin_email', :with => Faker::Internet.email
       fill_in 'admin_password', :with => "password"
       fill_in 'admin_password_confirmation', :with => "password"
-      save_and_open_page
+      # save_and_open_page
       
       click_button "Sign up"
-      save_and_open_page
+      # save_and_open_page
       #   fill_in 'admin_email', :with => Faker::Internet.email
       #   fill_in 'admin_password', :with => "password"
       #   click_button "Sign in"
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -29,15 +29,78 @@ feature "Events" do
       page.current_path.should == service_events_path(service)
       
       click_link "Update Status"
-      save_and_open_page
+      # save_and_open_page
       
       status = Fabricate(:status, :name => "Up")
       
       fill_in 'event[message]', :with => Faker::Lorem.word
       # select "Up", :from => 'event[status_id]'
       
-      page.execute_script("$('event[status_id]').val('Up')")
+      # page.execute_script("$('event[status_id]').val('Up')")
       
+    end
+    
+    scenario "should edit a service from the event page" do
+      admin = Fabricate(:admin)
+      visit services_path
+      fill_in 'admin_email', :with => admin.email
+      fill_in 'admin_password', :with => admin.password
+      click_button "Sign in"
+      
+      page.current_path.should == services_path
+      page.should have_content("Create a Service")
+      
+      click_link "Create a Service"
+      page.current_path.should == new_service_path
+      # service = Fabricate(:service)
+      
+      fill_in 'service[name]', :with => Faker::Lorem.word
+      fill_in 'service[description]', :with => Faker::Lorem.word
+      
+      click_button "Create Service"
+      
+      service = Service.last
+      click_link service.name
+      # save_and_open_page
+      
+      page.current_path.should == service_events_path(service)
+      click_link "Edit Service"
+      
+      page.current_path.should == edit_service_path(service)
+    
+    end
+    
+    scenario "should delete a service from the event page" do
+      admin = Fabricate(:admin)
+      visit services_path
+      fill_in 'admin_email', :with => admin.email
+      fill_in 'admin_password', :with => admin.password
+      click_button "Sign in"
+      
+      page.current_path.should == services_path
+      page.should have_content("Create a Service")
+      
+      click_link "Create a Service"
+      page.current_path.should == new_service_path
+      # service = Fabricate(:service)
+      
+      fill_in 'service[name]', :with => Faker::Lorem.word
+      fill_in 'service[description]', :with => Faker::Lorem.word
+      
+      click_button "Create Service"
+      
+      service = Service.last
+      click_link service.name
+      # save_and_open_page
+      
+      page.current_path.should == service_events_path(service)
+      click_link "Delete Service"
+      
+      page.current_path.should == services_path
+      # save_and_open_page
+      
+      # page.current_path.should == edit_service_path(service)
+    
     end
     
   

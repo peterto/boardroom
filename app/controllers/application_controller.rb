c7bbd2847dcc2f2a607884fcb5d450f97af348e5
Josh Laroff <josh@mserve.com>
2012-03-02 08:34:18 -0500

    Added the ability to send auth-token in HTTP header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   private
   def get_auth_token
-     if auth_token = params[:auth_token].blank? && request.headers["X-API-KEY"]
+     if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
          params[:auth_token] = auth_token
      end
   end

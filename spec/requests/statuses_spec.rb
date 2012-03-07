beae74a17d10274e9e41835836080e312d32b165
Peter To <pt9386@gmail.com>
2012-03-07 02:26:02 -0500

    Added requests specs for statuses for creation, updating and deletion of status

--- spec/requests/statuses_spec.rb
+++ spec/requests/statuses_spec.rb
@@ -6,7 +6,7 @@ feature "statuses" do
       visit root_path
       @admin = Fabricate(:admin, :email => Faker::Internet.email)
       
-      visit services_path
+      click_link "Admin"
       fill_in 'admin_email', :with => @admin.email
       fill_in 'admin_password', :with => @admin.password
       click_button "Sign in"
@@ -23,23 +23,69 @@ feature "statuses" do
       fill_in "status[name]", :with => name
       fill_in "status[description]", :with => description
       page.choose('status_image_big_upjpg')
-      # save_and
-      click_button "Create Status"
-      save_and_open_page
-      status = Status.last
       
+      expect {
+        click_button "Create Status"
+      }.to change { Status.count }.by(1)
+
+      status = Status.last      
       page.should have_content(status.name)
       page.current_path.should == statuses_path
       
     end
     
-    # scenario "logged in should update a status" do
-    # end
-    # 
-    # scenario "logged in should delete a status" do
-    # end
-    
-  end
-  
+    scenario "logged in, destroy a status" do
+      click_link "Statuses"
+      click_link "Create a Status"
+      name = Faker::Lorem.word
+      description = Faker::Lorem.sentence
+      
+      fill_in "status[name]", :with => name
+      fill_in "status[description]", :with => description
+      page.choose('status_image_big_upjpg')
+      click_button "Create Status"
 
+      status = Status.last      
+      page.should have_content(status.name)
+      page.current_path.should == statuses_path
+      
+      expect {
+        click_link "Delete"
+        page.driver.browser.switch_to.alert.accept
+      }.to change { Status.count }.by(-1)
+    
+    end
+    
+    scenario "logged in, should update a status" do
+      click_link "Statuses"
+      click_link "Create a Status"
+      name = Faker::Lorem.word
+      description = Faker::Lorem.sentence
+      
+      fill_in "status[name]", :with => name
+      fill_in "status[description]", :with => description
+      page.choose('status_image_big_upjpg')
+      
+      expect {
+        click_button "Create Status"
+      }.to change { Status.count }.by(1)
+      
+      status = Status.last      
+      page.should have_content(status.name)
+      page.current_path.should == statuses_path
+      
+      click_link "Edit"
+      name_new = Faker::Lorem.word
+      description_new = Faker::Lorem.sentence
+      
+      fill_in "status[name]", :with => name_new
+      fill_in "status[description]", :with => description_new
+      page.choose('status_image_upjpg')
+      click_button "Update Status"
+      
+      page.should have_content(name_new)
+      page.current_path.should == statuses_path
+      
+    end
+  end
 end

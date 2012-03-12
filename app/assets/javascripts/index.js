04db5f7ad764273a43ab4188c192a69a1a3fadef
Shaan Batra <sb1752@gmail.com>
2012-03-12 18:17:53 -0400

    prettier polling results

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,7 @@
 $(function() {
 	if ($("#all_statuses").length > 0) {
 		setTimeout(updateTableStatus, 5000);
-		$("#alert_message").click(function(){
+		$("#alert_message a").click(function(){
 			$("#alert_message").hide('fast');
 		});
 	}
@@ -12,20 +12,43 @@ function updateTableStatus () {
 	
 	$.getJSON(url, function(data) {
 		for (var key in data) {
+			var name = key;
+			
 			// First, get the image from the database
-			var image = "/assets/"+data[key][0].image;
+			var image = "/assets/"+data[name][0].image;
 			
 			// Now, make sure to remove white spaces because we do this on the front end
-			key = key.replace(/ /g, '');
+			id = name.replace(/ /g, '');
 			
 			// Check if the status image is different
-			element = $("#"+key+" img.image0");
+			element = $("#"+id+" img.image0");
 			if (element.attr("src") != image) {
 				element.attr("src", image);
+				$("#alert_message").hide();
+				$("#alert_message h4").text("Status has changed for "+name+"!");
+				$("#alert_message p").text(data[name][0].description);
+				
+				switch(data[name][0].status_name) {
+					case 'Up':
+					$("#alert_message").attr("class", "alert alert-success");
+					break;
+					
+					case 'Down':
+					$("#alert_message").attr("class", "alert alert-error");
+					break;
+					
+					case 'Warning':
+					$("#alert_message").attr("class", "alert");
+					break;
+					
+					default:
+					$("#alert_message").attr("class", "alert alert-info");
+				}
 				$("#alert_message").show('fast');
 			}
 		}
 		
 		setTimeout(updateTableStatus, 5000);
 	});
+	
 }
